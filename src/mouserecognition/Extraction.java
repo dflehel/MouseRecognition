35c2fe06ae532a3d31ffe41d5c66971bd45144fb
margitantal68 <margitantal68@gmail.com>
2019-01-07 11:05:28 +0200

    Extraction - januar 7.

--- src/mouserecognition/Extraction.java
+++ src/mouserecognition/Extraction.java
@@ -18,6 +18,11 @@ import static mouserecognition.Settings.NUM_ACTIONS;
  */
 public class Extraction {
 
+    private Queue<ArrayList<IEvent>> eventslist = new LinkedList<ArrayList<IEvent>>();
+    //itt megvan csinalva hogy apalajaban minden csak IFeatures interface tipusu osztalyokat fogad
+    private Queue<IFeature> moves = new LinkedList<IFeature>();
+    private ArrayList<IEvent> events = new ArrayList<>();
+    private String csvfile;
 
     public Queue<ArrayList<IEvent>> getEventslist() {
         return eventslist;
@@ -26,12 +31,6 @@ public class Extraction {
     public void setEventslist(Queue<ArrayList<IEvent>> eventslist) {
         this.eventslist = eventslist;
     }
-    
-    
-    private Queue<ArrayList<IEvent>> eventslist = new LinkedList<ArrayList<IEvent>>();
-    
-    //itt megvan csinalva hogy apalajaban minden csak IFeatures interface tipusu osztalyokat fogad
-    private Queue<IFeature> moves = new LinkedList<IFeature>();
 
     public Queue<IFeature> getMoves() {
         return moves;
@@ -40,51 +39,46 @@ public class Extraction {
     public void setMoves(Queue<IFeature> moves) {
         this.moves = moves;
     }
-    private ArrayList<IEvent> events = new ArrayList<>();
-    private String csvfile;
 
-    
-    public void featuresextraction(){
-        while (true){
-        if (this.eventslist.isEmpty()){
-            try {
-                synchronized (this.eventslist) {
-                this.eventslist.wait();}
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Extraction.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        else {
-             synchronized (this.eventslist) {
-            this.events = this.eventslist.poll();
-             }
-            //A csere eseten a megfelelo osztalyt kell peldanyositani
-            IFeature feature = null;
-            if (Settings.WHICH_FEATURES == 0){
-                feature = new LehellFeature();
-            }
-            else{
-                feature = new TeacherFeature();
-            }
-            //Annak meg kell hivni az extract featuret
-            feature.ExtractFeatures(events);
-           synchronized (this.moves) {
-            //majd a hjozza adas
-            this.moves.add( feature);
-           }
-            if (this.moves.size() ==NUM_ACTIONS){
-                synchronized (this.moves) {
-                    this.moves.notify();
+    public void featuresextraction() {
+        while (true) {
+            if (this.eventslist.isEmpty()) {
+                try {
+                    synchronized (this.eventslist) {
+                        this.eventslist.wait();
+                    }
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Extraction.class.getName()).log(Level.SEVERE, null, ex);
                 }
+            } else {
+                synchronized (this.eventslist) {
+                    this.events = this.eventslist.poll();
+                }
+                //A csere eseten a megfelelo osztalyt kell peldanyositani
+                IFeature feature = null;
+                if (Settings.WHICH_FEATURES == 0) {
+                    feature = new LehellFeature();
+                } else {
+
+                    feature = new TeacherFeature();
+                }
+                //Annak meg kell hivni az extract featuret
+                feature.ExtractFeatures(events);
+                synchronized (this.moves) {
+                    //majd a hjozza adas
+                    this.moves.add(feature);
+                }
+                if (this.moves.size() == NUM_ACTIONS) {
+                    synchronized (this.moves) {
+                        this.moves.notify();
+                    }
+                }
+
             }
-            
         }
-        }
-        
+
     }
-    
-    
-    
+
     public ArrayList<IEvent> getEvents() {
         return events;
     }
@@ -104,9 +98,8 @@ public class Extraction {
     public Extraction(String csvfile) {
         this.csvfile = csvfile;
     }
-     public Extraction() {
-    }
-    
-   
-}
 
+    public Extraction() {
+    }
+
+}
